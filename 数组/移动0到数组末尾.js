9aa48a844fb8fad4098032d528b58d7a924b3ca8
ichanghe <13716072325@163.com>
2023-02-27 22:26:19 +0800

    feat:双指针移动0到数组末尾

--- 数组/移动0到数组末尾.js
+++ 数组/移动0到数组末尾.js
@@ -12,5 +12,26 @@ function moveZero(arr) {
   }
 }
 let arr = [0, 2, 3, 4, 0, 99];
-moveZero(arr);
+moveZero2(arr);
 console.log(arr);
+
+// 双指针
+function moveZero2(arr) {
+  const length = arr.length;
+  if (length == 0) return;
+  let i;
+  let j = -1;
+  for (i = 0; i < length; i++) {
+    if (arr[i] == 0) {
+      if (j < 0) {
+        j = i;
+      }
+    }
+    if (arr[i] !== 0 && j >= 0) {
+      const n = arr[i];
+      arr[i] = arr[j];
+      arr[j] = n;
+      j++;
+    }
+  }
+}
