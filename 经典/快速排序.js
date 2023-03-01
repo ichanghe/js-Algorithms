cdb9aa92d5623aee38955025b340e4e3e26e1624
ichanghe <13716072325@163.com>
2023-03-01 22:59:12 +0800

    feat:快速排序

---
+++ 经典/快速排序.js
@@ -0,0 +1,19 @@
+function quickSort(arr) {
+  const length = arr.length;
+  if (length === 0) return arr;
+  const midIndex = Math.floor(length / 2);
+  const midValue = arr.splice(midIndex, 1)[0];
+
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const n = arr[i];
+    if (n < midValue) {
+      left.push(n);
+    } else {
+      right.push(n);
+    }
+  }
+  return quickSort(left).concat([midValue], quickSort(right));
+}
